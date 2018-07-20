16021c947d79a7af6aa53447ca45b1cf41340983
Kamil Rogalski <drootnar@gmail.com>
2018-07-20 10:57:40 +0200

    Add redis

--- packages/config/src/index.js
+++ packages/config/src/index.js
@@ -1,5 +1,6 @@
 export default {
   logLevel: process.env.LOG_LEVEL || 'NONE',
+  redisUrl: process.env.REDIS_URL || 'redis://redis:6379',
   api: {
     port: 5500
   }
--- packages/config/test/index.test.js
+++ packages/config/test/index.test.js
@@ -3,6 +3,7 @@ import config from '../src'
 describe('config', () => {
   it('returns dict with config values', async () => {
     expect(typeof config.logLevel).toBe('string')
+    expect(typeof config.redisUrl).toBe('string')
     expect(config.api).toMatchSnapshot()
   })
 })
