84873067f1f7539c23593467ad5ceeb68db115f4
Kamil Rogalski <drootnar@gmail.com>
2018-07-20 10:53:03 +0200

    Beggining of counterRouter

---
+++ packages/api/src/routers/counterRouter.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.status(200).send({})
+})
+
+router.post('/', (req, res) => {
+  res.status(200).send({})
+})
+
+export default router
--- packages/api/src/routes.js
+++ packages/api/src/routes.js
@@ -1,8 +1,11 @@
 import { Router } from 'express'
 
+import CounterRouter from './routers/counterRouter'
+
 const routes = Router()
 
 routes.get('', (req, res, next) => {
   res.send({ status: 'ok' })
 })
+routes.use('/v0/counter', CounterRouter)
 export default routes
